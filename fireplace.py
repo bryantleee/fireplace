4e160f4d603f41cb35fdd9069d445a0af0c81e88
Bryant Lee <bl692@cornell.edu>
2021-05-18 16:41:16 -0400

    add ember particles

--- fireplace.py
+++ fireplace.py
@@ -1,11 +1,11 @@
 import random
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 from collections import deque
 
 from PIL import Image
 import numpy as np
 
-def generate_fireplace_frame(fireplace_matrix: List[List[Tuple[int]]]) -> List[List[Tuple[int]]]:
+def generate_fireplace_frame(fireplace_matrix: List[List[Tuple[int]]], ember_locations: Dict) -> List[List[Tuple[int]]]:
     '''
     Generates a single frame of a fireplace, with each Tuple's representing an RGB pixel color
         - fireplace_matrix: 18 x 24 list of list of 0's
@@ -14,6 +14,9 @@ def generate_fireplace_frame(fireplace_matrix: List[List[Tuple[int]]]) -> List[L
     '''
     pixel_states = {j : 0 for j in range(len(fireplace_matrix[0]))}
 
+    # Each column can have 1 ember on the screen max at a time
+    ember_color = (194, 84, 35)
+    
     # formula to determine fire max height
     y = lambda x : -(1/15)*((x - 12)**2) + 12
 
@@ -33,8 +36,15 @@ def generate_fireplace_frame(fireplace_matrix: List[List[Tuple[int]]]) -> List[L
         1 : .40,
         0 : .45
     }
+
+    # move all embers up by one
+    for j in ember_locations:
+        if ember_locations[j] >= 0:
+            ember_locations[j] += 1
+        if ember_locations[j] > len(fireplace_matrix):
+            ember_locations[j] = -1
     
-    for i, row in enumerate(fireplace_matrix):
+    for i, row in enumerate((fireplace_matrix)):
         for j, pixel in enumerate(row):
             
             # max pixel height
@@ -47,6 +57,7 @@ def generate_fireplace_frame(fireplace_matrix: List[List[Tuple[int]]]) -> List[L
                 modifier = 0.1 if pixel_states.get(j - 1) == 2 else 0
                 to_kill = random.random() < death_chance[int(bound_delta)] + modifier
                 
+                # Each pixel has a chance of flickering (being black)
                 to_flicker = random.random() < 0.02
 
                 # Pixel is valid under certain conditions
@@ -56,12 +67,19 @@ def generate_fireplace_frame(fireplace_matrix: List[List[Tuple[int]]]) -> List[L
                 # only allow for one-pixel wide fire gaps
                 if pixel_states.get(j) == 1:
                     pixel_states[j] = 2
-                
-                # randomly determine if we allow for a pixel gap
-                if to_kill and pixel_states.get(j) != 2:
+
+                elif to_kill and pixel_states.get(j) != 2:
+                    # randomly determine if we allow for a pixel gap
                     pixel_states[j] = 1 if random.random() < 0.3 else 2
-    
-    return fireplace_matrix
+
+                    # sometimes a killed fire can make an ember
+                    if pixel_states[j] == 2 and ember_locations[j] == -1 and random.random() < 0.4:
+                        fireplace_matrix[i][j] = ember_color
+                        ember_locations[j] = i
+
+            if ember_locations[j] == i:
+                fireplace_matrix[i][j] = ember_color
+    return fireplace_matrix, ember_locations
 
 
 def generate_pixel_color(bound_delta: float) -> Tuple[int]:
@@ -83,11 +101,12 @@ def generate_pixel_color(bound_delta: float) -> Tuple[int]:
 
 
 if __name__ == '__main__':
+    fireplace_matrix = [[(0,0,0) for i in range(25)] for i in range(18)]
+    ember_locations = {j : -1 for j in range(len(fireplace_matrix[0]))}
     
-    for i in range(10):
+    for i in range(25):
         fireplace_matrix = [[(0,0,0) for i in range(25)] for i in range(18)]
-        fireplace_frame = generate_fireplace_frame(fireplace_matrix)
-        
+        fireplace_frame, ember_locations = generate_fireplace_frame(fireplace_matrix, ember_locations)
         # Convert the pixels into an array using numpy
         array = np.array(fireplace_frame[::-1], dtype=np.uint8)
 
