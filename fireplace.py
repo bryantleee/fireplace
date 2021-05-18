7a8daaaf35502ea20d3450d00d9ba4d335556164
Bryant Lee <bl692@cornell.edu>
2021-05-18 14:15:22 -0400

    update and add more comments

--- fireplace.py
+++ fireplace.py
@@ -5,12 +5,12 @@ from collections import deque
 from PIL import Image
 import numpy as np
 
-def generate_fireplace_frame(fireplace_matrix: List[List[int]]) -> List[List[int]]:
+def generate_fireplace_frame(fireplace_matrix: List[List[Tuple(int)]]) -> List[List[Tuple(int)]]:
     '''
-    Generates a single frame of a fireplace, with 1's representing a fire pixel present, and 0 representing no fire pixel present
+    Generates a single frame of a fireplace, with each Tuple's representing an RGB pixel color
         - fireplace_matrix: 18 x 24 list of list of 0's
     
-    Returns the a 18 x 24 list of list of 0's and 1's
+    Returns the a 18 x 24 list of list of Tuples of RGB pixel colors
     '''
     pixel_states = {j : 0 for j in range(len(fireplace_matrix[0]))}
 
@@ -64,18 +64,22 @@ def generate_fireplace_frame(fireplace_matrix: List[List[int]]) -> List[List[int
     return fireplace_matrix
 
 
-
-
 def generate_pixel_color(bound_delta: float) -> Tuple[int]:
+    '''
+    Generates the pixel color of the fire, with a different probability depending on region
+        
+        - bound_delta: distance between pixel and upper bound of the fire
+    
+    Returns a tuple of three integers in representing the RGB color of pixels
+    '''
     # possible pixel colors
     colors = deque([(251, 237, 83),(248, 221, 78), 
                     (246, 201, 73), (244, 183, 68), (255, 159, 56), (241, 146, 63)])
     
+    # change probabilities depending on distance to the upper bound
     colors.rotate((int(bound_delta) + 1) // 2)
     return random.choices(colors, weights=[50, 25, 15, 10, 0, 0])[0]
-    # print(int(bound_delta), colors[0])
     # return random.choices(colors, weights=[100, 0, 0, 0, 0, 0])[0]
-    
 
 
 if __name__ == '__main__':
@@ -84,7 +88,7 @@ if __name__ == '__main__':
         fireplace_matrix = [[(0,0,0) for i in range(25)] for i in range(18)]
         fireplace_frame = generate_fireplace_frame(fireplace_matrix)
         
-    #   Convert the pixels into an array using numpy
+        # Convert the pixels into an array using numpy
         array = np.array(fireplace_frame[::-1], dtype=np.uint8)
 
         # Use PIL to create an image from the new array of pixels
