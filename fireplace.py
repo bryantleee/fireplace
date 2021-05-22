4aecb2a0a1b2ba645e796603e67d6f5b9d99b8cb
Bryant Lee <bl692@cornell.edu>
2021-05-22 11:28:47 -0400

    reorganize and add support for palette

--- fireplace.py
+++ fireplace.py
@@ -13,7 +13,9 @@ import imageio
 
 
 class Fireplace:    
-    def generate_fireplace_frame(self, fireplace_matrix: List[List[Tuple[int]]], ember_locations: Dict, blember_locations: Dict) -> List[List[Tuple[int]]]:
+    def generate_fireplace_frame(self, fireplace_matrix: List[List[Tuple[int]]], 
+        ember_locations: Dict, blember_locations: Dict, palette=((251, 237, 83),(248, 221, 78), 
+                        (246, 201, 73), (244, 183, 68), (255, 159, 56), (241, 146, 63))) -> List[List[Tuple[int]]]:
         '''
         Generates a single frame of a fireplace, with each Tuple's representing an RGB pixel color
             - fireplace_matrix: 18 x 24 list of list of 0's
@@ -80,7 +82,7 @@ class Fireplace:
 
                     # Pixel is valid under certain conditions
                     if i <= bound and not to_kill and pixel_states.get(j) != 2 and not to_flicker and not blember_locations[j] == i:
-                        fireplace_matrix[i][j] = self.generate_pixel_color(bound_delta)
+                        fireplace_matrix[i][j] = self.generate_pixel_color(bound_delta, palette)
                     
                     # only allow for one-pixel wide fire gaps
                     if pixel_states.get(j) == 1:
@@ -100,7 +102,7 @@ class Fireplace:
         return fireplace_matrix, ember_locations, blember_locations
 
     
-    def generate_pixel_color(self, bound_delta: float) -> Tuple[int]:
+    def generate_pixel_color(self, bound_delta: float, palette) -> Tuple[int]:
         '''
         Generates the pixel color of the fire, with a different probability depending on region
             
@@ -109,9 +111,8 @@ class Fireplace:
         Returns a tuple of three integers in representing the RGB color of pixels
         '''
         # possible pixel colors
-        colors = deque([(251, 237, 83),(248, 221, 78), 
-                        (246, 201, 73), (244, 183, 68), (255, 159, 56), (241, 146, 63)])
-        weights = deque([50, 25, 15, 10, 0, 0])
+        colors = deque(palette)
+        weights = (50, 25, 15, 10, 0, 0)
 
         # change probabilities depending on distance to the upper bound
         colors.rotate((int(bound_delta) + 1) // 2)
